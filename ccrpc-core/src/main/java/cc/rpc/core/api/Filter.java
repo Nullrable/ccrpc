299dafd6e5712596dbd6306fefc891cbc81a5e2d
卢苏东 <nhsoft.lsd@hotmail.com>
2024-03-29 23:20:13 +0800

    add filter

--- ccrpc-core/src/main/java/cc/rpc/core/api/Filter.java
+++ ccrpc-core/src/main/java/cc/rpc/core/api/Filter.java
@@ -6,7 +6,7 @@ package cc.rpc.core.api;
 public interface Filter {
 
     /**
-     * 返回不等于null 直接返回 ，如果return null filter 继续往下走。 //TODO nhsoft.lsd 这个实现方式不是很好理解
+     * 返回不等于null 直接返回 ，如果return null filter 继续往下走。 //TODO nhsoft.lsd 这个实现方式不是很好理解，可以改为责任链模式
      * @param request 请求
      * @return 响应结果
      */
